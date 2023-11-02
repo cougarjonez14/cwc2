a55979936192777374bd57fe7fb8441f06f70d33
stationAstral <cougarjonez@gmail.com>
2023-11-02 08:14:37 -0700

    Initial Commit

---
+++ cwc2/cwc2App.swift
@@ -0,0 +1,17 @@
+//
+//  cwc2App.swift
+//  cwc2
+//
+//  Created by stationAstral on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct cwc2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
